197318c195933b35690ba63371752a53f9c161e0
tomide1 <cleriocuzio15@yahoo.com>
2018-02-06 18:00:37 +0000

    remove unused dependencies and workaround for handling mapping of css files although that should be done by default (revisit commit)

--- src/main/java/org/researchobject/roshow/WebConfig.java
+++ src/main/java/org/researchobject/roshow/WebConfig.java
@@ -19,6 +19,7 @@ public class WebConfig extends WebMvcConfigurerAdapter{
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        //registry.addResourceHandler("/static/css/**").addResourceLocations("classpath:/resources/static/css/");
     }
 
 }
